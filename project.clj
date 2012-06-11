c01e7f4c78d008edfd5c86fc28f05c18d05c7296
Alexander Clare <alexander.clare@gmail.com>
2012-06-11 14:24:57 -0500

    CLJS code comparison functions

--- project.clj
+++ project.clj
@@ -4,13 +4,11 @@
                            [noir "1.3.0-beta8"]]
             :dev-dependencies [[lein-cljsbuild "0.2.1"]]
             :cljsbuild
-            {
-             :source-path "src/trapunto/cljs"
-             :compiler
-             {
-              :output-to "resources/public/js/cljs.js"
-              :optimizations :simple
-              :pretty-print true
-              }
-             }
+            {:builds
+             [{:source-path "src/trapunto/cljs/"
+               :compiler
+               {:output-to "resources/public/js/cljs.js"
+                :optimizations :simple
+                :pretty-print true}}]}
             :main trapunto.server)
+
