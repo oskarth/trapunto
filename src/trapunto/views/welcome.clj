dabd0a88d18311d3fa713a27a72cce364a666855
Oskar Thoren <o.thoren@gmail.com>
2012-06-11 14:55:57 -0400

    add class to text-area for js hookup

--- src/trapunto/views/welcome.clj
+++ src/trapunto/views/welcome.clj
@@ -8,9 +8,11 @@
   (common/layout
    [:p "Welcome to trapunto"]
    (form-to [:post "/"]
-            (text-area "code" "")
+            (text-area {:class "code"} "code" "")
             (submit-button "Submit"))
   [:img {:src "/img/trapunto1.jpg" :width "80" :height "80" :float "left"}]))
 
 (defpage [:post "/"] {:keys [code]}
+  ;; take code, pipe to quil in sandox
+  ;; then use that image to refresh it only on the same page
   (println code))
