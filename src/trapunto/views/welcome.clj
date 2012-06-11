7f2da5fb7ccb4bfd12db834de4a84048ebf08dac
Oskar Thoren <o.thoren@gmail.com>
2012-06-11 14:02:47 -0400

    add POST text-area, prints text

--- src/trapunto/views/welcome.clj
+++ src/trapunto/views/welcome.clj
@@ -1,9 +1,16 @@
 (ns trapunto.views.welcome
-  (:require [trapunto.views.common :as common]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
-
+  (:require [trapunto.views.common :as common])
+  (:use [noir.core :only [defpage defpartial]]
+        [hiccup.core :only [html]]
+        [hiccup.form :only [form-to text-area submit-button]]))
+               
 (defpage "/" []
-         (common/layout
-           [:p "Welcome to trapunto"]))
+  (common/layout
+   [:p "Welcome to trapunto"]
+   (form-to [:post "/"]
+            (text-area "code" "")
+            (submit-button "Submit"))
+  [:img {:src "/img/trapunto1.jpg" :width "80" :height "80" :float "left"}]))
+
+(defpage [:post "/"] {:keys [code]}
+  (println code))
