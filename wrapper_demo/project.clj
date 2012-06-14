15c71b38f984beae4981d33a95e6ac441c9420b2
Alexander Clare <alexander.clare@gmail.com>
2012-06-14 16:22:01 -0500

    Wrapper for Quil that accepts text-string input
    The wrapper is just a demonstration and needs to be implemented in the
    main program. It also avoids launching the Processing applet and ignores
    exceptions.

---
+++ wrapper_demo/project.clj
@@ -0,0 +1,8 @@
+(defproject quilwrap "0.1.0-SNAPSHOT"
+            :description "FIXME: write this!"
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [quil "1.4.1"]
+                           [org.clojars.processing-core/org.processing.core "1.5.1"]]
+            :dev-dependencies [[lein-cljsbuild "0.2.1"]]
+            :main quilwrap)
+
