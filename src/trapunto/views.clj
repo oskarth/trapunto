ad839deed786cf509ac4fc42b662dde1d479df02
Oskar Thoren <o.thoren@gmail.com>
2012-06-18 11:45:30 -0400

    hide utility textbox, remove submit-button

--- src/trapunto/views.clj
+++ src/trapunto/views.clj
@@ -6,6 +6,9 @@
         [hiccup.form :only [form-to text-area submit-button]]
         [hiccup.page :only [include-js include-css html5]]))
 
+;; put in codemirror
+
+
 (defn index-page []
   (html5
    [:head
@@ -16,7 +19,6 @@
     [:h1 "Trapunto"]
     (form-to [:post "/"]
              (text-area {:id "visible"} "code" "")
-             (text-area {:id "invisible"} "invis" "")
-             (submit-button "Submit"))
+             (text-area {:id "invisible" :hidden "true"} "invis" ""))
     [:img {:id "output" :src "/img/trapunto1.jpg" :width "80" :height "80" :float "left"}]
     [:script {:type "text/javascript"} "trapunto.client.timer(1000);"]]))
