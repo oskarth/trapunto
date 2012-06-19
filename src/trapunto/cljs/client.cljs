9f7ef74e88c4660d419a9af7b972ae1d4c04c75d
Oskar Thoren <o.thoren@gmail.com>
2012-06-19 12:40:14 -0400

    codemirror tied to textbox

--- src/trapunto/cljs/client.cljs
+++ src/trapunto/cljs/client.cljs
@@ -1,41 +1,48 @@
-(ns trapunto.client)
+(ns trapunto.client
+ (:use [jayq.util :only [map->js]]))
 
-;; TODO: howdy world use text as src
-;; TODO: eval text (+ 3 4) and use as src
-;; TODO: replace filename with saved image
-;; TODO: fix bad image reloaded (marked region?)
+	;; TODO: howdy world use text as src
+	;; TODO: eval text (+ 3 4) and use as src
+	;; TODO: replace filename with saved image
+	;; TODO: fix bad image reloaded (marked region?)
 
 (declare compare-code)
 
-(defn ^:export timer [delay-ms]
-  (js/setInterval compare-code delay-ms))
+	(defn ^:export timer [delay-ms]
+	 (js/setInterval compare-code delay-ms))
+
+	(defn ^:export codemirror []
+	 (js/CodeMirror.fromTextArea (.getElementById js/document "visible")
+	  (map->js {:mode "text/x-clojure"})))
+	;; from ibdknox live-cljs, need more?
 
 (def xhr (new js/XMLHttpRequest))
 
-(defn replace-image []
-  (let [filename "x"]
-    ;; check if response is OK and finished loading
-    (if (and (= (. xhr -readyState) 4)
-             (= (. xhr -status) 200)
-             (do
-               ;; temporary testing value
-               (set! (.. js/document (getElementById "visible") -value)
-                     (str (. xhr -response)))
+	(defn replace-image []
+	 (let [filename "x"]
+	  ;; check if response is OK and finished loading
+	  (if (and (= (. xhr -readyState) 4)
+	       (= (. xhr -status) 200)
+	       (do
+		;; temporary testing value
+		(set! (.. js/document (getElementById "visible") -value)
+		 (str (. xhr -response)))
 
-               ;; actual image replacement code
-               (comment (.. js/document (getElementById "output")
-                            (setAttribute
-                             "src" (str "/img/" (str filename) ".jpg")))))))))
+		;; actual image replacement code
+		(comment (.. js/document (getElementById "output")
+			  (setAttribute
+			   "src" (str "/img/" (str filename) ".jpg")))))))))
 
-(defn compare-code []
-  (let [visible (.. js/document (getElementById "visible") -value)]
-    (if-not (= visible (.. js/document (getElementById "invisible") -value))
-      (do (set! (.. js/document (getElementById "invisible") -value)  visible)
-          (doto xhr
-            (. open "POST" "/" false) 
-            (. setRequestHeader "Content-Type" "text/plain")
+	(defn compare-code []
+	 (let [visible (.. js/document (getElementById "visible") -value)]
+	  (if-not (= visible (.. js/document (getElementById "invisible") -value))
+	   (do (set! (.. js/document (getElementById "invisible") -value)  visible)
+	    (doto xhr
+	     (. open "POST" "/" false) 
+	     (. setRequestHeader "Content-Type" "text/plain")
 
-            ;; Placeholder for generated image URL; random number to make it
-            ;;   easy to detect success when testing
-            (. send (. js/Math random)))
-          (set! (. xhr -onreadystatechange) replace-image)))))
+	     ;; Placeholder for generated image URL; random number to make it
+	     ;;   easy to detect success when testing
+	     ;;(. send (. js/Math random)))
+	    (. send "(+ 4 5)"))
+	   (set! (. xhr -onreadystatechange) replace-image)))))
--- src/trapunto/views.clj
+++ src/trapunto/views.clj
@@ -1,24 +1,25 @@
 (ns trapunto.views
   (:use
-   ;; [noir.core :only [defpage defpartial]]
-   ;; [noir.response :only [empty json]]
-        [hiccup.core :only [html]]
-        [hiccup.form :only [form-to text-area submit-button]]
-        [hiccup.page :only [include-js include-css html5]]))
-
-;; put in codemirror
-
+    ;; [noir.core :only [defpage defpartial]]
+    ;; [noir.response :only [empty json]]
+    [hiccup.core :only [html]]
+    [hiccup.form :only [form-to text-area submit-button]]
+    [hiccup.page :only [include-js include-css html5]]))
 
 (defn index-page []
   (html5
-   [:head
-    [:title "Trapunto"]
-    (include-css "/css/reset.css")
-    (include-js "/js/cljs.js")]
-   [:body
-    [:h1 "Trapunto"]
-    (form-to [:post "/"]
-             (text-area {:id "visible"} "code" "")
-             (text-area {:id "invisible" :hidden "true"} "invis" ""))
-    [:img {:id "output" :src "/img/trapunto1.jpg" :width "80" :height "80" :float "left"}]
-    [:script {:type "text/javascript"} "trapunto.client.timer(1000);"]]))
+    [:head
+     [:title "Trapunto"]
+     (include-css "/css/reset.css")
+     (include-css "/css/codemirror.css")
+     (include-js "/js/codemirror.js")
+     (include-js "/js/clojure.js")
+     (include-js "/js/cljs.js")]
+    [:body
+     [:h1 "Trapunto"]
+     (form-to [:post "/"]
+              (text-area {:id "visible"} "code" "")
+              (text-area {:id "invisible" :hidden "true"} "invis" ""))
+     [:img {:id "output" :src "/img/trapunto1.jpg" :width "80" :height "80" :float "left"}]
+     [:script {:type "text/javascript"} "trapunto.client.timer(1000);"]
+     [:script {:type "text/javascript"} "trapunto.client.codemirror();"]]))
