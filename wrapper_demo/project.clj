e4cfbea54c4891af3edb4698ba078e2d9f3962c4
Alexander Clare <alexander.clare@gmail.com>
2012-06-21 15:22:29 -0500

    Small changes to the development environment

--- wrapper_demo/project.clj
+++ wrapper_demo/project.clj
@@ -3,6 +3,8 @@
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [quil "1.4.1"]
                            [org.clojars.processing-core/org.processing.core "1.5.1"]]
-            :dev-dependencies [[lein-cljsbuild "0.2.1"]]
+            :dev-dependencies [[lein-swank "1.4.4"]
+                               [lein-cljsbuild "0.2.1"]]
+            :exclusions [swank-clojure]
             :main quilwrap)
 
