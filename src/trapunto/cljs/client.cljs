1186242c8d9838db46782e5c2d95a588f0c50055
Alexander Clare <alexander.clare@gmail.com>
2012-06-22 00:31:06 -0500

    Processing wrapper and image push

--- src/trapunto/cljs/client.cljs
+++ src/trapunto/cljs/client.cljs
@@ -1,10 +1,5 @@
 (ns trapunto.client)
 
-;; TODO: howdy world use text as src
-;; TODO: eval text (+ 3 4) and use as src
-;; TODO: replace filename with saved image
-;; TODO: fix bad image reloaded (marked region?)
-
 (declare compare-code)
 
 (defn ^:export timer [delay-ms]
@@ -13,19 +8,13 @@
 (def xhr (new js/XMLHttpRequest))
 
 (defn replace-image []
-  (let [filename "x"]
-    ;; check if response is OK and finished loading
-    (if (and (= (. xhr -readyState) 4)
-             (= (. xhr -status) 200)
-             (do
-               ;; temporary testing value
-               (set! (.. js/document (getElementById "visible") -value)
-                     (str (. xhr -response)))
-
-               ;; actual image replacement code
-               (comment (.. js/document (getElementById "output")
-                            (setAttribute
-                             "src" (str "/img/" (str filename) ".jpg")))))))))
+  ;; check if response is OK and finished loading
+  (if (and (= (. xhr -readyState) 4)
+           (= (. xhr -status) 200))
+    (.. js/document (getElementById "output")
+        ;; TODO Image URLs hit the page and refresh before the image is
+        ;;   finished writing, preventing the image from displaying at all
+       (setAttribute "src" (str "/img/" (str (. xhr -response)) ".png")))))
 
 (defn compare-code []
   (let [visible (.. js/document (getElementById "visible") -value)]
@@ -34,8 +23,5 @@
           (doto xhr
             (. open "POST" "/" false) 
             (. setRequestHeader "Content-Type" "text/plain")
-
-            ;; Placeholder for generated image URL; random number to make it
-            ;;   easy to detect success when testing
-            (. send (. js/Math random)))
+            (. send visible))
           (set! (. xhr -onreadystatechange) replace-image)))))
--- src/trapunto/draw.clj
+++ src/trapunto/draw.clj
@@ -1,68 +1,75 @@
-;; maybe use Runnable to launch this instead
-;;   of running it in a separate program?
-
-(ns trapunto
+(ns trapunto.draw
   (:use [quil.core]
-        [clojail.core]
-        [clojail.testers :only [secure-tester]]
+        [clojure.string :only [split]]
         [clj-time.core :only [now]]
-        [clj-time.coerce :only [to-long]]))
+        [clj-time.coerce :only [to-long]]
+        
+        ;; Eventually implement Clojail and rewrite long loops to help prevent
+        ;;   security problems
+        #_[clojail.core]
+        #_[clojail.testers :only [secure-tester]])
+  (import [processing.core PApplet]))
 
-;;(def *output-prefix*)
+;;; Interleave two sequences, padding the smaller one. *Not lazy*
+(defn padded-interleave-two [c1 c2 pad]
+  (let [cc1 (count c1)
+        cc2 (count c2)]
+    (if (> cc1 cc2)
+      (interleave c1 (into c2 (repeat (- cc1 cc2) pad)))
+      (interleave (into c1 (repeat (- cc2 cc1) pad)) c2))))
 
-;; maybe parse out defns from user input and insert them here
-;;   to be used in "main", i.e. draw
+;;; Extract all of the functions corresponding to Processing functions from Quil
+(def processing-funs
+  (loop [m {}
+         fs (map first (filter #(:processing-name (meta (second %)))
+                               (ns-publics 'quil.core)))]
+    (if (empty? fs) m
+        (recur (assoc m (str (first fs))
+                      (str ". trapunto.draw/g " (first fs)))
+               (rest fs)))))
 
-(defn setup []
-  ;; just called once at startup
-  ;; set up black on white, 1FPS, static image
-  (smooth)
-  (frame-rate 1)
-  (background 255)
-  (stroke 0)
-  (fill 0)
-  (no-loop))
-
-;; define random name
-(defn- gen-name []
-  (str (to-long (now))))
-
-;; TODO: extract file name
-;; TODO: enable clojail process
-
-;; insert draw before base-level code, and save after it
-(defn draw []
-  (let [name (gen-name)]
-    ;;  ((sandbox secure-tester) ;; toggle this to enable sandbox, repl
-    ;; insert user code here
-    
-    (stroke (random 255))             ;;Set the stroke colour to a random grey
-    (stroke-weight (random 10))       ;;Set the stroke thickness randomly
-    (fill (random 255))               ;;Set the fill colour to a random grey
-    
-    (let [diam (random 100)           ;;Set the diameter to a value between 0 and 100
-          x    (random (width))       ;;Set the x coord randomly within the sketch
-          y    (random (height))]     ;;Set the y coord randomly within the sketch
-      (ellipse x y diam diam)))       ;;Draw a circle at x y with the correct diameter
-  
-  ;;   ) ;; jail time ends here
-  (save (str "resources/public/img/" name ".png")))) ;; insert file name to upload here
-;; here we want to send back name to user
+;;; Replace a Processing function with its equivalent or leave it alone
+(defn replace-processing [regex-result]
+  (let [name (second regex-result)
+        replacement (get processing-funs name)]
+    (str "("  (if (nil? replacement) name replacement))))
 
 
-;; run this to generate image
-(defsketch example
-  :setup setup
-  :draw draw
-  :target :none
-  :size [200 200])
+(defn draw-image [input [width height] filename]
+  (let [context (. (PApplet.) createGraphics width height PApplet/P2D)]
+    ;; Stick our PGraphics object that corresponds to the replaced functions
+    ;;   above in the namespace 
+    (intern 'trapunto.draw 'g context)
+    (. context beginDraw)
+    ;; Write a blank white image by default
+    (. context background 255 255 255)
+    (let [pattern (re-pattern #"\( *([^\"\( ]*)")
+          ;; Wrap the whole of the input in a do statement for user convenience
+          result (str "(do "
+                      ;; Interleave replaced function names with the text in
+                      ;;   between, i.e. arguments and parentheses
+                      (apply str (padded-interleave-two
+                                  (map replace-processing
+                                       (re-seq pattern input))
+                                  (split input pattern) "")) ")")]
+      (try
+        (eval (read-string result))
+        (. context endDraw)
+        (. context save filename)
+        ;;; TODO: Do not save a file when an exception is caught from draw-image,
+        ;;;   and do not change the image on the server!
+        #_(catch Exception ex)
+        (finally
+         (. context endDraw))))))
 
-(comment
-  ;; on the server...
-  (binding [*ns* 'temp]
-    (try
-      (load-string (recieve-from-server-function))  ;; input from server
-      (catch java.lang.Exception e
-        (don't-save-function) ;; push forward an empty draw function
-                              ;;   with no saving, no refreshing image
-        ))))
+(defn gen-name [] (to-long (now)))
+
+;;; TODO: Do not save a file when an exception is caught from draw-image,
+;;;   and do not change the image on the server!
+(defn output-image [instream]
+  (let [filename (str (gen-name))
+        input (slurp instream)]
+    (. (Thread. #(draw-image
+                  input [200 200]
+                  (str "resources/public/img/" filename ".png"))) start)
+    filename))
